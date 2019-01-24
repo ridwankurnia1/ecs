9110e7fe2ff36185ee5419d9517cccddf306dfe1
Ridwan Kurniawan <ridwankur@gmail.com>
2019-01-24 15:22:14 +0700

    Final Api Connection

--- app/src/main/java/id/amfg/ecs/LoginActivity.java
+++ app/src/main/java/id/amfg/ecs/LoginActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import id.amfg.ecs.api.ApiUtils;
 import id.amfg.ecs.api.CoreApiInterface;
 import id.amfg.ecs.model.LoginModel;
+import id.amfg.ecs.model.User;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -30,6 +31,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -37,11 +40,9 @@ import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "ridwan:123456", "bar@example.com:world"
-    };
-
-    //private UserLoginTask mAuthTask = null;
+//    private static final String[] DUMMY_CREDENTIALS = new String[]{
+//            "ridwan:123456", "bar@example.com:world"
+//    };
 
     // UI references.
     private EditText mUseridView;
@@ -56,8 +57,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mContext = this;
-        mUseridView = (EditText) findViewById(R.id.userid);
-        mPasswordView = (EditText) findViewById(R.id.password);
+        mUseridView = findViewById(R.id.userid);
+        mPasswordView = findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
@@ -69,7 +70,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        Button mbtnLogin = (Button) findViewById(R.id.btnLogin);
+        Button mbtnLogin = findViewById(R.id.btnLogin);
         mbtnLogin.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -81,94 +82,51 @@ public class LoginActivity extends AppCompatActivity {
         mProgressView = findViewById(R.id.login_progress);
     }
 
-//    private void attemptLogin() {
-//        showProgress(true);
-//
-//        // Reset errors.
-//        mUseridView.setError(null);
-//        mPasswordView.setError(null);
-//
-//        // Store values at the time of the login attempt.
-//        String userid = mUseridView.getText().toString();
-//        String password = mPasswordView.getText().toString();
-//
-//        boolean cancel = false;
-//        View focusView = null;
-//
-//        // Check for a valid password, if the user entered one.
-//        if (TextUtils.isEmpty(password)) {
-//            mPasswordView.setError(getString(R.string.error_invalid_password));
-//            focusView = mPasswordView;
-//            cancel = true;
-//        }
-//
-//        // Check for a valid email address.
-//        if (TextUtils.isEmpty(userid)) {
-//            mUseridView.setError(getString(R.string.error_field_required));
-//            focusView = mUseridView;
-//            cancel = true;
-//        }
-//
-//        if (cancel) {
-//            // There was an error; don't attempt login and focus the first
-//            // form field with an error.
-//            focusView.requestFocus();
-//        } else {
-//            coreApi.login(
-//                    "application/json"
-//                    , new LoginModel(userid, password))
-//                    .enqueue(new Callback<ResponseBody>() {
-//                        @Override
-//                        public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-//                            if (response.isSuccessful()) {
-//                                showProgress(false);
-//                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                                startActivity(intent);
-//                                try {
-//                                    JSONObject jsonRESULTS = new JSONObject(response.body().string());
-//                                    if (jsonRESULTS.getString("error").equals("false")) {
-//                                        Toast.makeText(mContext, "BERHASIL LOGIN", Toast.LENGTH_SHORT).show();
-//                                        //String nama = jsonRESULTS.getJSONObject("user").getString("nama");
-//                                        Intent intent = new Intent(mContext, MainActivity.class);
-//                                        //intent.putExtra("result_nama", nama);
-//                                        startActivity(intent);
-//                                    } else {
-//                                        // Jika login gagal
-//                                        String error_message = "LOGIN GAGAL";
-//                                        Toast.makeText(mContext, error_message, Toast.LENGTH_SHORT).show();
-//                                    }
-//                                } catch (JSONException e) {
-//                                    e.printStackTrace();
-//                                } catch (IOException e) {
-//                                    e.printStackTrace();
-//                                }
-//                            } else {
-//                                showProgress(false);
-//                                Toast.makeText(mContext, response.message(), Toast.LENGTH_LONG).show();
-//                            }
-//                        }
-//
-//                        @Override
-//                        public void onFailure(Call<ResponseBody> call, Throwable t) {
-//                            Log.e("debug", "onFailure: ERROR > " + t.toString());
-//                            showProgress(false);
-//                        }
-//                    });
-//        }
-//    }
-
     private void attemptLogin() {
-        boolean mCancel = this.loginValidation();
-        if (mCancel) {
-            focusView.requestFocus();
-        } else {
-            String userid = mUseridView.getText().toString();
-            String password = mPasswordView.getText().toString();
+        boolean cancel = loginValidation();
 
-            //loginProcessWithRetrofit(userid, password);
-            Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
-            loginIntent.putExtra("USER", userid);
-            startActivity(loginIntent);
+        if (cancel) {
+            focusView.requestFocus();
+        }
+        else {
+            showProgress(true);
+            CoreApiInterface coreApi = ApiUtils.getCoreApi();
+            final String userid = mUseridView.getText().toString();
+            String password = mPasswordView.getText().toString();
+            coreApi.login(
+                    "application/json"
+                    , new LoginModel(userid, password))
+                    .enqueue(new Callback<ResponseBody>() {
+                        @Override
+                        public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                            if (response.isSuccessful()) {
+                                try {
+                                    showProgress(false);
+                                    if (response.body() != null){
+                                        JSONObject jsonRESULTS = new JSONObject(response.body().string());
+                                        User userObj = new Gson().fromJson(jsonRESULTS.toString(), User.class);
+
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        intent.putExtra("user", userObj.getZuusna());
+                                        startActivity(intent);
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            } else {
+                                showProgress(false);
+                                Toast.makeText(mContext, "Invalid credential, please check your user id or password", Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<ResponseBody> call, Throwable t) {
+                            Log.e("debug", "onFailure: ERROR > " + t.toString());
+                            showProgress(false);
+                        }
+                    });
         }
     }
 
@@ -198,137 +156,33 @@ public class LoginActivity extends AppCompatActivity {
         return cancel;
     }
 
-    private void loginProcessWithRetrofit(final String userid, String password) {
-        CoreApiInterface coreApi = ApiUtils.getCoreApi();
-        coreApi.login(
-                "application/json"
-                , new LoginModel(userid, password))
-                .enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        if (response.isSuccessful()) {
-                            Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
-                            loginIntent.putExtra("USER", userid);
-                            startActivity(loginIntent);
-                        }
-                    }
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-                    @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-                        call.cancel();
-                        Toast.makeText(LoginActivity.this, "Please check your network connection and internet permission", Toast.LENGTH_LONG).show();
-                    }
-                });
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
     }
-
-//    private void attemptLogin() {
-//        if (mAuthTask != null) {
-//            return;
-//        }
-//
-//        if (cancel) {
-//            // There was an error; don't attempt login and focus the first
-//            // form field with an error.
-//            focusView.requestFocus();
-//        } else {
-//            // Show a progress spinner, and kick off a background task to
-//            // perform the user login attempt.
-//            showProgress(true);
-//            mAuthTask = new UserLoginTask(userid, password);
-//            mAuthTask.execute((Void) null);
-//        }
-//    }
-
-//    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-//    private void showProgress(final boolean show) {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-//            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-//
-//            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-//            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-//                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-//                @Override
-//                public void onAnimationEnd(Animator animation) {
-//                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-//                }
-//            });
-//
-//            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-//            mProgressView.animate().setDuration(shortAnimTime).alpha(
-//                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-//                @Override
-//                public void onAnimationEnd(Animator animation) {
-//                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-//                }
-//            });
-//        } else {
-//            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-//            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-//        }
-//    }
-
-//    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-//
-//        private final String mUserId;
-//        private final String mPassword;
-//
-//        UserLoginTask(String UserId, String password) {
-//            mUserId = UserId;
-//            mPassword = password;
-//        }
-//
-//
-//        Boolean result;
-//        @Override
-//        protected Boolean doInBackground(Void... params) {
-//            coreApi.login(mUserId, mPassword)
-//                    .enqueue(new Callback<ResponseBody>() {
-//                        @Override
-//                        public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-//                            if (response.isSuccessful()) {
-//                                result = true;
-//                            } else {
-//                                result = false;
-//                            }
-//                        }
-//
-//                        @Override
-//                        public void onFailure(Call<ResponseBody> call, Throwable t) {
-//                            Log.e("debug", "onFailure: ERROR > " + t.toString());
-//                        }
-//                    });
-//
-////            for (String credential : DUMMY_CREDENTIALS) {
-////                String[] pieces = credential.split(":");
-////                if (pieces[0].equals(mUserId)) {
-////                    // Account exists, return true if the password matches.
-////                    return pieces[1].equals(mPassword);
-////                }
-////            }
-//
-//            return result;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(final Boolean success) {
-//            mAuthTask = null;
-//            showProgress(false);
-//
-//            if (success) {
-//                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                startActivity(intent);
-//            } else {
-//                mPasswordView.setError(getString(R.string.error_incorrect_password));
-//                mPasswordView.requestFocus();
-//            }
-//        }
-//
-//        @Override
-//        protected void onCancelled() {
-//            mAuthTask = null;
-//            showProgress(false);
-//        }
-//    }
 
 }
 
--- app/src/main/java/id/amfg/ecs/MainActivity.java
+++ app/src/main/java/id/amfg/ecs/MainActivity.java
@@ -3,6 +3,7 @@ package id.amfg.ecs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String strUser = getIntent().getStringExtra("user");
+        Toast.makeText(this, "Welcome " + strUser.toUpperCase(), Toast.LENGTH_SHORT).show();
     }
 }
