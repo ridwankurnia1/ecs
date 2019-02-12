5eaf39026730c74bed775a7b659dc0aee291d2a7
Ridwan Kurniawan <ridwankur@gmail.com>
2019-02-12 13:55:22 +0700

    Add Tab Layout

---
+++ app/src/main/java/id/amfg/ecs/BracketActivity.java
@@ -0,0 +1,14 @@
+package id.amfg.ecs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BracketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bracket);
+    }
+}
